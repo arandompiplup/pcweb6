8a16e9fdc988a26fcf23d5290ec7e7c9b6062900
arandompiplup <arandompiplup@gmail.com>
2024-08-06 10:38:52 +0800

    added files for add details and update, aded nav to app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import PostPageHome from './views/PostPageHome';
 import LoginPage from './views/LoginPage';
 import SignUpPage from './views/SignUpPage';
+import PostPageAdd from './views/PostPageAdd';
+import PostPageUpdate from './views/PostPageUpdate';
+import PostPageDetails from './views/PostPageDetails';
 
 function App() {
 
@@ -11,7 +14,10 @@ function App() {
     [
       {path:"/", element:<PostPageHome />},
       {path:"/login", element:<LoginPage />},
-      {path:"/signup", element:<SignUpPage />}
+      {path:"/signup", element:<SignUpPage />},
+      {path:"/add", element:<PostPageAdd />},
+      {path:"/post/:id", element:<PostPageDetails/>},
+      {path:"/update/:id", element:<PostPageUpdate/>}
     ]
   )
 
